ab65cd7d07f88c40044620cd245646e429cee2cf
Tariq Blecher <tariq.blecher@gmail.com>
2016-03-04 11:02:13 +0200

    test

--- src/scatterbrane_test.py
+++ src/scatterbrane_test.py
@@ -1,48 +1,28 @@
-from __future__ import division
-
-from cProfile import run
-
-from brane import Brane
-import Pyxis
 from Pyxis.ModSupport import *
 import numpy as np
 from fits_work import EasyFits
 import pyfits
-import pyrap.tables as pt
 from parameter_work import *
-import glob
+import kliko
 
-"""Repacking of 'Scatterbrane', see:http://krosenfeld.github.io/scatterbrane/current/user/brane.html"""
 
-deg2muas = 3600e6
-pc2m = 3.086e16
+def run_test(config):
+    schema_string = open('../kliko.yml', 'r').read()
+    parameters = open(config, 'r').read()
+    schema = kliko.validate_kliko(schema_string)
 
-def run_scatterbrane(config):
+    kliko.validate_parameters(parameters, schema)
 
-    parameters=load_json_parameters_into_dictionary(config)
-    ism_dict = setup_keyword_dictionary('ism_', parameters)
-    v.MS = parameters['fitsfolder']+'.MS'
-    v.OUTDIR = parameters['fitsfolder']
-    x.sh('mkdir '+OUTDIR)
-    fits_list = np.sort(glob.glob('input/%s/*.fits' % parameters['fitsfolder'])).tolist()
-    model0 = EasyFits(fits_list[0])
+    parameters = load_json_parameters_into_dictionary(config)
+    v.OUTDIR = parameters['fits'].split('.fits')[0]
+    x.sh('mkdir '+ OUTDIR)
+    fits = 'input/'+parameters['fits']
+    model0 = EasyFits(fits)
+    new = model0.data+np.random.normal(0, parameters['scale'], size=model0.data.shape)
+    pyfits.writeto(OUTDIR+"/scattered.fits", new,
+                   pyfits.getheader(model0.fits.filename()), clobber=True)
 
-    brane = Brane(model0.fits[0].data[0, 0, :, :], model0.pixelsize * deg2muas,
-                  nphi=2**ism_dict['nphi_exponent'], wavelength=parameters['wavelength'])
-    brane.generatePhases()
-    brane.scatter()
+    x.sh('mv %s output/' % OUTDIR)
 
-    header = pyfits.getheader(model0.fits.filename())
-    header['NAXIS1'] = brane.nx
-    header['NAXIS2'] = brane.nx
-    header['CDELT1'] = -1*brane.dx/float(deg2muas)
-    header['CDELT2'] = 1*brane.dx/float(deg2muas)
 
-    pyfits.writeto(OUTDIR+"/scattered.fits", brane.iss.reshape(1, 1, brane.nx, brane.nx),
-                   header, clobber=True)
-
-    x.sh('mv %s output/'%OUTDIR)
-
-    print "ISM Scattered"
-
-run_scatterbrane('parameters.json')
+run_test('input/parameters.json')
